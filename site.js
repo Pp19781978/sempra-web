6585e6da4a7b5e66d8fd0d344769f762e543be8f
Pp19781978 <ppppttttpppp@gmail.com>
2026-08-25 16:23:24 +0200

    Redesign site and correct factual content

---
+++ site.js
@@ -0,0 +1,13 @@
+(function () {
+  document.querySelectorAll('[data-year]').forEach(function (el) {
+    el.textContent = new Date().getFullYear();
+  });
+
+  document.querySelectorAll('.mobile-menu a').forEach(function (link) {
+    link.addEventListener('click', function () {
+      var menu = link.closest('details');
+      if (menu) menu.removeAttribute('open');
+    });
+  });
+})();
+
